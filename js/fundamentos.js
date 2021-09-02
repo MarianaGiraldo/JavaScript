e05926c536375719a98e7bd5dfe09b80561cef29
MarianaGiraldo <mgiraldo594@misena.edu.co>
2021-09-02 07:45:54 -0500

    First commit

---
+++ js/fundamentos.js
@@ -0,0 +1,14 @@
+//Variables
+var nombre = "Mariana"; //Declara una variable
+let edad = 17; //Declara una variable local con ámbito de bloque
+const pi = 3.1416; //Declara un nombre de constante de solo lectura y ámbito de bloque.
+
+//Funciones
+//1. Función sin parámetros y sin retorno
+function saludar(){
+    alert("Hola Mundo...");
+}
+function nomFuncion(){
+    console.log("Aquí va un texto por consola");
+    document.write("Texto en el documento");
+}
